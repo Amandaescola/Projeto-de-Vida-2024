9a73cb51d4f367761f3de0e190863acb22b95c91
Amanda Escola <108474141+Amandaescola@users.noreply.github.com>
2024-02-16 09:23:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll(".botao");
+
+for(let i-0;i <botoes.length++){
+    botoes[i].onclick = function(){
+        botoe[i].classList.add("ativo");
+    }
+    console.log();
+}
